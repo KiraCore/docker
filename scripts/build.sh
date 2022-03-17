fee8796f5e7e18e53f711f82485e0092c93a4b57
asmodat <asmodat@gmail.com>
2022-03-17 06:26:46 +0000

    branch sub

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,21 +6,12 @@ set -x
 WORKDIR=$PWD
 UTILS_VER=$(utilsVersion 2> /dev/null || echo "")
 
-# Installing utils is essential to simplify the setup steps
-if [[ $(versionToNumber "$UTILS_VER" || echo "0") -lt $(versionToNumber "v0.0.15" || echo "1") ]] ; then
-    echo "INFO: KIRA utils were NOT installed on the system, setting up..." && sleep 2
-    KIRA_UTILS_BRANCH="v0.0.3" && cd /tmp && rm -fv ./i.sh && \
-    wget https://raw.githubusercontent.com/KiraCore/tools/$KIRA_UTILS_BRANCH/bash-utils/install.sh -O ./i.sh && \
-    chmod 555 ./i.sh && ./i.sh "$KIRA_UTILS_BRANCH" "/var/kiraglob" && . /etc/profile && loadGlobEnvs
-else
-    echoInfo "INFO: KIRA utils are up to date, latest version $UTILS_VER"
-fi
-
 cd $WORKDIR
 
 CORS_DOCKERFILE=./cors-anywhere/Dockerfile
 
-REF_GITHUB_BRNACH=$(git rev-parse --abbrev-ref HEAD)
+REF_GITHUB_BRNACH=$(git rev-parse --abbrev-ref HEAD || echo "")
+( [ -z "$REF_GITHUB_BRNACH" ] || [ "${REF_GITHUB_BRNACH,,}" == "head" ] ) && REF_GITHUB_BRNACH="${SOURCE_BRANCH}"
 
 rm -fv $CORS_DOCKERFILE
 REF_GITHUB_BRNACH=${REF_GITHUB_BRNACH//"/"/"\/"}
