46d018a43332b9a80799dd4f1accd429b00bfef9
Asmodat <asmodat@gmail.com>
2022-05-26 13:29:23 +0000

    Use global common direcotry as default glob store

--- kira-base/container/start.sh
+++ kira-base/container/start.sh
@@ -6,9 +6,9 @@ KIRA_SETUP_VER=$(globGet KIRA_SETUP_VER "$GLOBAL_COMMON_RO")
 
 echoInfo "INFO: Staring $NODE_TYPE container $KIRA_SETUP_VER ..."
 
-HALT_TASK=$(globGet HALT_TASK $GLOBAL_COMMON) && [ "${HALT_TASK,,}" != "true" ] && HALT_TASK="false"
-EXIT_TASK=$(globGet EXIT_TASK $GLOBAL_COMMON) && [ "${EXIT_TASK,,}" != "true" ] && EXIT_TASK="false"
-CFG_TASK=$(globGet CFG_TASK $GLOBAL_COMMON) && [ "${CFG_TASK,,}" == "true" ] && CFG_TASK="false"
+HALT_TASK=$(globGet HALT_TASK) && [ "${HALT_TASK,,}" != "true" ] && HALT_TASK="false"
+EXIT_TASK=$(globGet EXIT_TASK) && [ "${EXIT_TASK,,}" != "true" ] && EXIT_TASK="false"
+CFG_TASK=$(globGet CFG_TASK) && [ "${CFG_TASK,,}" == "true" ] && CFG_TASK="false"
 timerStart "catching_up"
 timerStart "success"
 
@@ -18,19 +18,19 @@ if ($(isNaturalNumber $RESTART_COUNTER)) ; then
     globSet RESTART_TIME "$(date -u +%s)"
 fi
 
-while [ "$HALT_TASK" == "true" ] || [ "$EXIT_TASK" == "false" ] ; do
+while [ "$HALT_TASK" == "true" ] || [ "$EXIT_TASK" == "true" ] ; do
     if [ "$EXIT_TASK" == "true" ] ; then
         echoInfo "INFO: Ensuring that sekaid & interxd processes are killed"
-        globSet HALT_TASK true $GLOBAL_COMMON
+        globSet HALT_TASK true
         pkill -15 sekaid || echoWarn "WARNING: Failed to kill sekaid"
         pkill -15 interxd || echoWarn "WARNING: Failed to kill interxd"
-        globSet EXIT_TASK false $GLOBAL_COMMON
+        globSet EXIT_TASK false
     fi
     echoInfo "INFO: Waiting for container to be unhalted..."
     sleep 30
 done
 
-globSet CFG_TASK true $GLOBAL_COMMON
+globSet CFG_TASK true
 FAILED="false"
 if [ "${NODE_TYPE,,}" == "sentry" ] || [ "${NODE_TYPE,,}" == "seed" ]; then
     $COMMON_DIR/sentry/start.sh || FAILED="true"
@@ -43,7 +43,7 @@ else
     FAILED="true"
 fi
 
-globSet CFG_TASK false $GLOBAL_COMMON
+globSet CFG_TASK false
 if [ "${FAILED,,}" == "true" ] ; then
     echoErr "ERROR: $NODE_TYPE node startup failed"
     sleep 3
