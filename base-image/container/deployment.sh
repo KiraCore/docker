d9a477d31a155144f327b0636936e692fd6e73de
Asmodat <asmodat@gmail.com>
2022-09-04 07:07:34 +0000

    update tools

--- base-image/container/deployment.sh
+++ base-image/container/deployment.sh
@@ -12,7 +12,7 @@ FLUTTER_CHANNEL="stable"
 FLUTTER_VERSION="3.0.4-$FLUTTER_CHANNEL"
 DART_CHANNEL_PATH="stable/release"
 DART_VERSION="2.17.5"
-TOOLS_VERSION="v0.2.19"
+TOOLS_VERSION="v0.2.20"
 
 echo "Starting core dependency build..."
 apt-get update -y > ./log || ( cat ./log && exit 1 )
