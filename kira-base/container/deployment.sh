bcf5f91088baa047f96c03db70a0c0680523b899
mrlutik <yaievgeniy@gmail.com>
2023-07-11 23:40:23 +0200

    Change kira-img interx ver v0.4.34 -> v0.4.35

--- kira-base/container/deployment.sh
+++ kira-base/container/deployment.sh
@@ -9,7 +9,7 @@ apt-get install -y --allow-unauthenticated --allow-downgrades --allow-remove-ess
 
 BTC_VERSION="24.0.1"
 SEKAI_VERSION="v0.3.17"
-INTERX_VERSION="v0.4.34"
+INTERX_VERSION="v0.4.35"
 TOOLS_VERSION="v0.3.46"
 COSIGN_VERSION="v2.0.2"
 
